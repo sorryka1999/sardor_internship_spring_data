1a5d4b032d601e87b1afe425c6e2c3ce78fae77b
sardor <sorryka1999@gmail.com>
2022-07-25 09:25:02 +0500

    commiting-DB_connection_test

--- src/test/java/megafon/internship/controllers/PostgresqlConnectionTest.java
+++ src/test/java/megafon/internship/controllers/PostgresqlConnectionTest.java
@@ -1,7 +1,26 @@
 package megafon.internship.controllers;
 
-//@Testcontainers
-//аннотация не работает
+import megafon.internship.repository.EmployeesRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+@SpringBootTest
 public class PostgresqlConnectionTest {
+
+    @Autowired
+    private EmployeesRepo employeesRepo;
+
+    @Test
+    public void testingConnection() {
+        //тестируем подключение к БД sardor_internship
+        //если сможем взять данные из таблицца Employees то значит есть подключение
+        //в данный момент не имеет значение что возвращает метод findAll()
+        //мы только проверяем тип возвращаемого объекта
+        Assertions.assertEquals(employeesRepo.findAll().getClass(), ArrayList.class);
+    }
 
 }
